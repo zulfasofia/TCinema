1236cc0c7b589db36c52acebe942b89e06302221
Zulfa <77313918+zulfasofia@users.noreply.github.com>
2021-10-30 19:56:21 +0700

    Delete dvd_rent directory

--- dvd_rent/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  // connect database mysql
-  $connect = mysqli_connect("localhost", "root", "", "dvd_rent");
-
-  if(mysqli_connect_errno()){
-    echo "Failed to Connect to MySQL :" . mysqli_connect_error();
-  }
-?>
--- dvd_rent/home.php
+++
@@ -1,2 +0,0 @@
-<center><br><br><br><br><br>
-<img src="assets/images/logo.png" width="400px" height="400px"/> <br>
